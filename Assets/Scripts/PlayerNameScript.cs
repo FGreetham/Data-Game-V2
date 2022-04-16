995dec17e32e0bcc4960c35d34a7e74e07c76a1b
FGreetham <francescagreetham@gmail.com>
2022-04-16 17:39:25 +0100

    Update PlayerNameScript.cs
    Fixed PlayerPrefs Has Key and Get String

--- Assets/Scripts/PlayerNameScript.cs
+++ Assets/Scripts/PlayerNameScript.cs
@@ -3,29 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEngine.UI;
+
 
 public class PlayerNameScript : MonoBehaviour
 {
     [SerializeField] private GameObject startButton;
 
     public GameObject inputField;
-
     public string inputText;
-    // public string PlayerName; //Key for the player prefs. Surely this shouldn't be needed?
 
     private void Start()
     {
-        // if(PlayerPrefs.HasKey(PlayerName))
-        //Why does this not recognise Key 'PlayerName' from SetString method?
-
-        if (PlayerPrefs.HasKey(PlayerName) == true)
+        //Pulls the PlayerPrefs/PlayerName that was previously entered and puts it in the InputField
+        if (PlayerPrefs.HasKey("PlayerName"))
         {
-            PlayerPrefs.GetString(PlayerName);
-            Debug.Log("Player name is" + PlayerName);
-            //If I use variable above - it will come back with this HasKey debug.log
-            //So it thinks something is set to the PlayerPrefs...
-            //But it won't show the "PlayerName" text.
+            Debug.Log("Player name is " + PlayerPrefs.GetString("PlayerName"));
+            
+            inputField.GetComponent<TMP_InputField>().text = PlayerPrefs.GetString("PlayerName");
         }
         else
             Debug.Log("No player name");
@@ -35,32 +29,17 @@ public class PlayerNameScript : MonoBehaviour
     public void SaveName6()
     {
         string inputText = inputField.GetComponent<TMP_InputField>().text;
-        Debug.Log(inputText);
 
-        PlayerPrefs.SetString(PlayerName, inputText);
-        //Have put this here as the SetString method below doesn't seem to work
-        //Only works if I make a seperate PlayerName variable like above. 
+        //This SetString and GetString is working in the console
+        PlayerPrefs.SetString("PlayerName", inputText);
+        Debug.Log("GetString is " + PlayerPrefs.GetString("PlayerName"));
 
-
-        // SceneManager.LoadScene("Garden Scene");
+        SceneManager.LoadScene("Garden Scene");
     }
-
-    public void SetString(string PlayerName, string inputText)
-    {
-        PlayerPrefs.SetString(PlayerName, inputText);
-        //ATTENTION
-        //Something here isn't working.. it doesn't pull to a GetString method (returns 'void')
-        //and the Key 'PlayerName' is not recognised for HasKey either.
-        //Also how do I know if it's connected to my local variable 'inputText' from the SaveName6 method
-        //and it hasn't just created a new 'inputText' variable for itself?
-    }
-
 
     //Only shows the Start button which calls the SaveName method when player begins typing
     public void ButtonShow()
     {
         startButton.SetActive(true);
     }
-
-
 }
